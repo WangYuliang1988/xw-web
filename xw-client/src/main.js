14179e6a3ed091a31e8eec20762905c2397b6083
Yuliang Wang <langzi1206@msn.cn>
2022-10-11 15:05:28 +0800

    xw-client 工程增加 router 插件

--- xw-client/src/main.js
+++ xw-client/src/main.js
@@ -3,9 +3,11 @@ import 'mutationobserver-shim'
 import './plugins/bootstrap-vue'
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
